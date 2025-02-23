b39257ba32e25af0aa2703a67da0fa1355862f57
Shubham Dhole <shubhamdhole215@gmail.com>
2025-02-23 14:44:18 +0530

    installed react-native-vector-icons

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import { SafeAreaView, StyleSheet, Text} from 'react-native';
 import React from 'react';
 import { getFontFamily } from '../utils/fontFamily';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={[styles.headerText, {fontFamily:getFontFamily(true,'bold')}]}>HomeScreen</Text>
+      <Text style={[styles.headerText, {fontFamily:getFontFamily(true,'medium')}]}>HomeScreen</Text>
+      <Icon name="close" size= {30}/>
     </SafeAreaView>
   );
 };
